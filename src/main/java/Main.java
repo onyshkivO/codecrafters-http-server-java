3194ff485a24bf4b4f36f918f3781430836fb165
Ostapko58 <prostoostap58@gmail.com>
2024-06-22 10:11:30 +0300

    Respond with body #CN2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,24 +22,24 @@ public class Main {
             out = new PrintWriter(clientSocket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            for (int i = 0; i < 1; i++) {
-                String request = in.readLine();
-                String[] reqParts = request.split("\r\n");
-                String reqLine = reqParts[0];
-                String path = reqLine.substring(reqLine.indexOf(' '), reqLine.lastIndexOf(' ')).trim();
-                Pattern pattern = Pattern.compile("\\/echo\\/(.*)");
-                Matcher matcher = pattern.matcher(path);
-                if (matcher.matches()) {
-                    String message = matcher.group(1);
-                    String response = String.format("HTTP/1.1 200 OK\\r\\n\\r\\n" +
-                            "Content-Type: text/plain\\r\\n" +
-                            "Content-Length: %d\\r\\n\\r\\n%s\\r\\n", message.length(), message);
-                    out.print(response);
-                } else {
-                    out.print("HTTP/1.1 404 Not Found\r\n\r\n");
-                }
-                out.flush();
+
+            String request = in.readLine();
+            String[] reqParts = request.split("\r\n");
+            String reqLine = reqParts[0];
+            String path = reqLine.substring(reqLine.indexOf(' '), reqLine.lastIndexOf(' ')).trim();
+            Pattern pattern = Pattern.compile("\\/echo\\/(.*)");
+            Matcher matcher = pattern.matcher(path);
+            if (matcher.matches()) {
+                String message = matcher.group(1);
+                String response = String.format("HTTP/1.1 200 OK\\r\\n\\r\\n" +
+                        "Content-Type: text/plain\\r\\n" +
+                        "Content-Length: %d\\r\\n\\r\\n%s", message.length(), message);
+                out.print(response);
+            } else {
+                out.print("HTTP/1.1 404 Not Found\r\n\r\n");
             }
+            out.flush();
+
             out.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
