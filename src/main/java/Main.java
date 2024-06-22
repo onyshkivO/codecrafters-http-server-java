79b7a944ed053d3f85ef7d6c14ae47590025d2ed
Ostapko58 <prostoostap58@gmail.com>
2024-06-22 21:20:07 +0300

    Respond with body #CN2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,10 +43,9 @@ public class Main {
                         message.length() + "\r\n\r\n" + message);
             } else if (path.startsWith("/user-agent")) {
                 String userAgent = headers.get("User-Agent");
-//                String format = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", userAgent.length(), userAgent);
-                System.out.println(userAgent.length());
-//                System.out.println(format);
-                out.print("HTTP/1.1 200 OK\r\n\r\n");
+                String format = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", userAgent.length(), userAgent);
+                System.out.println(format);
+                out.print(format);
 //                out.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
 //                        userAgent.length() + "\r\n\r\n" + userAgent);
 
