1198729bda5824f639df6bbd1eeed62c7143969f
Ostapko58 <prostoostap58@gmail.com>
2024-06-22 10:27:41 +0300

    Read header #FS3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,28 +16,30 @@ public class Main {
         BufferedReader in;
         try {
             serverSocket = new ServerSocket(4221);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept(); // Wait for connection from client.
             out = new PrintWriter(clientSocket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-
             String request = in.readLine();
             String[] reqParts = request.split("\r\n");
+            String[] reqParts2 = request.split("\r\n\n");
+            System.out.println(Arrays.toString(reqParts));
+            System.out.println(Arrays.toString(reqParts2));
             String reqLine = reqParts[0];
             String path = reqLine.substring(reqLine.indexOf(' '), reqLine.lastIndexOf(' ')).trim();
-            Pattern pattern = Pattern.compile("\\/echo\\/(.*)");
-            Matcher matcher = pattern.matcher(path);
-            if ("/".equals(path.trim())){
+
+            if ("/".equals(path)) {
                 out.print("HTTP/1.1 200 OK\r\n\r\n");
-            }
-            if (matcher.matches()) {
-                String message = matcher.group(1);
+            } else if (path.startsWith("/echo")) {
+                String message = path.split("/")[2];
                 out.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
                         message.length() + "\r\n\r\n" + message);
-            } else {
+            } else if (path.startsWith("/user-agent")){
+
+
+            }
+            else {
                 out.print("HTTP/1.1 404 Not Found\r\n\r\n");
             }
             out.flush();
