e25d6aec7326c4cc01a0e47ca23321e1d3241a6c
Ostapko58 <prostoostap58@gmail.com>
2024-06-22 10:13:22 +0300

    Respond with body #CN2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,7 +33,7 @@ public class Main {
                 String message = matcher.group(1);
                 String response = String.format("HTTP/1.1 200 OK\\r\\n\\r\\n" +
                         "Content-Type: text/plain\\r\\n" +
-                        "Content-Length: %d\\r\\n\\r\\n%s", message.length(), message);
+                        "Content-Length: %d\\r\\n\\r\\n"+message, message.length());
                 out.print(response);
             } else {
                 out.print("HTTP/1.1 404 Not Found\r\n\r\n");
