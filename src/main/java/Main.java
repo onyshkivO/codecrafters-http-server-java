1f14b4d0b030b7d8933910c20b7ceb7411573cc1
Ostapko58 <prostoostap58@gmail.com>
2024-06-22 10:39:26 +0300

    Read header #FS3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,6 +45,7 @@ public class Main {
                         message.length() + "\r\n\r\n" + message);
             } else if (path.startsWith("/user-agent")) {
                 String userAgent = request.substring(reqLine.indexOf("User-Agent:")).trim();
+                System.out.println(userAgent);
                 out.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
                         userAgent.length() + "\r\n\r\n" + userAgent);
             } else {
