943adb51301727ed35860d8870adc1718b75320a
Ostapko58 <prostoostap58@gmail.com>
2024-06-22 09:26:24 +0300

    Extract URL path #IH0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,15 +9,29 @@ public class Main {
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
-
+        PrintWriter out;
+        BufferedReader in;
         try {
             serverSocket = new ServerSocket(4221);
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept(); // Wait for connection from client.
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            out.print("HTTP/1.1 200 OK\r\n\r\n");
+            out = new PrintWriter(clientSocket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            for (int i = 0; i < 2; i++) {
+                String request = in.readLine();
+                String[] reqParts = request.split("\r\n");
+                String reqLine = reqParts[0];
+                String path = reqLine.substring(reqLine.indexOf(' '), reqLine.indexOf(' ', reqLine.indexOf(' ') + 1));
+                if ("/".equals(path)) {
+                    out.print("HTTP/1.1 200 OK\r\n\r\n");
+                } else {
+                    out.print("HTTP/1.1 404 Not Found\r\n\r\n");
+                }
+                out.flush();
+            }
             out.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
