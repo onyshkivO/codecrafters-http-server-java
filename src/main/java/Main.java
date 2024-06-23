92112cf6ce51d1dd175f2a3e38d4051c9e15e419
Ostapko58 <prostoostap58@gmail.com>
2024-06-23 12:00:56 +0300

    Concurrent connections #EJ5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     static ServerSocket serverSocket;
-    static Socket clientSocket;
+//    static Socket clientSocket;
     static PrintWriter out;
     static BufferedReader in;
     static Pattern headerPattern = Pattern.compile("([\\w-]+): (.*)");
@@ -25,10 +25,11 @@ public class Main {
         try {
             init();
             ExecutorService executorService = Executors.newCachedThreadPool();
-            while(true){
-                executorService.submit(()-> {
+
+            while (true) {
+                executorService.submit(() -> {
                     try {
-                        processRequest();
+                        test();
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -40,54 +41,87 @@ public class Main {
         }
     }
 
-    private static void processRequest() throws IOException {
-        parseRequest();
+//    private static void processRequest() throws IOException {
+//        parseRequest();
+//
+//        String path = reqLine.substring(reqLine.indexOf(' '), reqLine.lastIndexOf(' ')).trim();
+//
+//        sendResponse(path);
+//        out.flush();
+//        out.close();
+//    }
+//
+//    private static void sendResponse(String path) {
+//        if ("/".equals(path)) {
+//            out.print("HTTP/1.1 200 OK\r\n\r\n");
+//        } else if (path.startsWith("/echo")) {
+//            String message = path.split("/")[2];
+//            out.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
+//                    message.length() + "\r\n\r\n" + message);
+//        } else if (path.startsWith("/user-agent")) {
+//            String userAgent = headers.get("User-Agent");
+//            String format = String.format("HTTP/1.1 200 OK\r\n" +
+//                    "Content-Type: text/plain\r\n" +
+//                    "Content-Length: %d\r\n\r\n%s", userAgent.length(), userAgent);
+//            out.print(format);
+//        } else {
+//            out.print("HTTP/1.1 404 Not Found\r\n\r\n");
+//        }
+//    }
+//
+    private static void init() throws IOException {
+        serverSocket = new ServerSocket(4221);
+        serverSocket.setReuseAddress(true);
 
-        String path = reqLine.substring(reqLine.indexOf(' '), reqLine.lastIndexOf(' ')).trim();
-
-        sendResponse(path);
-        out.flush();
-        out.close();
     }
+//
+//    private static void parseRequest() throws IOException {
+//        out = new PrintWriter(clientSocket.getOutputStream());
+//        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//        reqLine = in.readLine();
+//        String line = in.readLine();
+//        while (line != null && !line.isBlank()) {
+//            Matcher headerMatcher = headerPattern.matcher(line);
+//            if (headerMatcher.matches()) {
+//                headers.put(headerMatcher.group(1), headerMatcher.group(2));
+//            }
+//            line = in.readLine();
+//        }
+//    }
 
-    private static void sendResponse(String path) {
+
+    private static void test() throws IOException {
+        Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+        PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String reqLine = bufferedReader.readLine();
+        String line = bufferedReader.readLine();
+        Map<String, String> headers = new HashMap<>();
+        while (line != null && !line.isBlank()) {
+            Matcher headerMatcher = headerPattern.matcher(line);
+            if (headerMatcher.matches()) {
+                headers.put(headerMatcher.group(1), headerMatcher.group(2));
+            }
+            line = bufferedReader.readLine();
+        }
+        String path = reqLine.substring(reqLine.indexOf(' '), reqLine.lastIndexOf(' ')).trim();
         if ("/".equals(path)) {
-            out.print("HTTP/1.1 200 OK\r\n\r\n");
+            printWriter.print("HTTP/1.1 200 OK\r\n\r\n");
         } else if (path.startsWith("/echo")) {
             String message = path.split("/")[2];
-            out.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
+            printWriter.print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
                     message.length() + "\r\n\r\n" + message);
         } else if (path.startsWith("/user-agent")) {
             String userAgent = headers.get("User-Agent");
             String format = String.format("HTTP/1.1 200 OK\r\n" +
                     "Content-Type: text/plain\r\n" +
                     "Content-Length: %d\r\n\r\n%s", userAgent.length(), userAgent);
-            out.print(format);
+            printWriter.print(format);
         } else {
-            out.print("HTTP/1.1 404 Not Found\r\n\r\n");
+            printWriter.print("HTTP/1.1 404 Not Found\r\n\r\n");
         }
+        printWriter.flush();
+        printWriter.close();
     }
-
-    private static void init() throws IOException {
-        serverSocket = new ServerSocket(4221);
-        serverSocket.setReuseAddress(true);
-
-    }
-
-    private static void parseRequest() throws IOException {
-        clientSocket = serverSocket.accept(); // Wait for connection from client.
-        out = new PrintWriter(clientSocket.getOutputStream());
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        reqLine = in.readLine();
-        String line = in.readLine();
-        while (line != null && !line.isBlank()) {
-            Matcher headerMatcher = headerPattern.matcher(line);
-            if (headerMatcher.matches()) {
-                headers.put(headerMatcher.group(1), headerMatcher.group(2));
-            }
-            line = in.readLine();
-        }
-    }
-
 
 }
