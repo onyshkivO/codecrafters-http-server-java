fd87706402da58995a4512861cc4fb284c028954
Ostapko58 <prostoostap58@gmail.com>
2024-06-23 13:37:56 +0300

    Read request body #QV8

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -2,14 +2,10 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Connection {
     private Socket clientSocket;
@@ -20,6 +16,8 @@ public class Connection {
     private Map<String, String> headers = new HashMap<>();
     private String reqBody = null;
     private String[] args;
+    private String method;
+    private String path;
 
     public Connection(ServerSocket socket, String[] args) throws IOException {
         clientSocket = socket.accept();
@@ -30,12 +28,16 @@ public class Connection {
         out = new PrintWriter(clientSocket.getOutputStream());
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         reqLine = in.readLine();
+        path = reqLine.substring(reqLine.indexOf(' '), reqLine.lastIndexOf(' ')).trim();
+        method = reqBody.substring(0, reqLine.indexOf(' ')).trim();
         Matcher headerMatcher;
         String line = in.readLine();
         while (line != null && !line.isBlank()) {
             headerMatcher = headerPattern.matcher(line);
             if (headerMatcher.matches()) {
                 headers.put(headerMatcher.group(1), headerMatcher.group(2));
+            } else if (!line.isBlank()) {
+                reqBody = line;
             }
             line = in.readLine();
         }
@@ -47,7 +49,7 @@ public class Connection {
     }
 
     private void process() throws IOException {
-        String path = reqLine.substring(reqLine.indexOf(' '), reqLine.lastIndexOf(' ')).trim();
+
         if ("/".equals(path)) {
             out.print("HTTP/1.1 200 OK\r\n\r\n");
         } else if (path.startsWith("/echo")) {
@@ -63,18 +65,17 @@ public class Connection {
                     "Content-Length: %d\r\n\r\n%s", userAgent.length(), userAgent);
             out.print(response);
         } else if (path.startsWith("/files/")) {
-            String fileName = path.split("/")[2];
             String directory = args[1];
-            File file = new File(directory + fileName);
-            if (file.exists()) {
-                String fileContent = new String(Files.readAllBytes(file.toPath()));
-                String response = String.format("HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: application/octet-stream\r\n" +
-                        "Content-Length: %d\r\n\r\n%s", fileContent.length(), fileContent);
-                out.print(response);
-            } else {
-                out.print("HTTP/1.1 404 Not Found\r\n\r\n");
+            String fileName = path.split("/")[2];
+            switch (method) {
+                case "GET":
+                    getFile(directory + fileName);
+                    break;
+                case "POST":
+                    createFile(directory + fileName);
+                    break;
             }
+            getFile(path);
         } else {
             out.print("HTTP/1.1 404 Not Found\r\n\r\n");
         }
@@ -82,4 +83,25 @@ public class Connection {
         out.close();
     }
 
+    private void createFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        file.createNewFile();
+        FileWriter myWriter = new FileWriter(file);
+        myWriter.write(reqBody);
+        myWriter.close();
+        out.print("HTTP/1.1 201 Created\r\n\r\n");
+    }
+
+    private void getFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        if (file.exists()) {
+            String fileContent = new String(Files.readAllBytes(file.toPath()));
+            String response = String.format("HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: application/octet-stream\r\n" +
+                    "Content-Length: %d\r\n\r\n%s", fileContent.length(), fileContent);
+            out.print(response);
+        } else {
+            out.print("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+    }
 }
